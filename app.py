0714efabe055f5cfb378f6a3f7b2d910ada4fe72
vs-tries-to-code <vishnusree1805@gmail.com>
2026-04-10 19:36:15 +0530

    Displaying qr, download, and customize color

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", show_qr=False)
 
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.form["data"]
-    qr = qrcode.QRCode(
-        border = 2,
-        box_size= 3,
-        )
+    fill_color = request.form["fill-color"]
+    back_color = request.form["back-color"]
+    qr = qrcode.QRCode(border = 2, box_size= 3)
     qr.add_data(data)
     qr.make(fit=True)
-    img = qr.make_image()
+    img = qr.make_image(fill_color=fill_color, back_color=back_color)
     img.save("static/your-qr.png")
-    return "QR successfully generated!"
+    return render_template("index.html", show_qr=True)
 
 app.run(debug=True)
