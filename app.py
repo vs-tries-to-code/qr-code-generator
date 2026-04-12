5927e21f22dad6e2cf215e08891ce3b8e953faa4
vs-tries-to-code <vishnusree1805@gmail.com>
2026-04-12 11:10:56 +0530

    Enhanced UI

--- app.py
+++ app.py
@@ -9,12 +9,17 @@ def home():
 
 @app.route("/generate", methods=["POST"])
 def generate():
+    #fetching the inputted values from html form 
     data = request.form["data"]
     fill_color = request.form["fill-color"]
     back_color = request.form["back-color"]
+
+    #creating qr using QRCode object
     qr = qrcode.QRCode(border = 2, box_size= 3)
     qr.add_data(data)
     qr.make(fit=True)
+
+    #making image of the qr with the custom colour
     img = qr.make_image(fill_color=fill_color, back_color=back_color)
     img.save("static/your-qr.png")
     return render_template("index.html", show_qr=True)
